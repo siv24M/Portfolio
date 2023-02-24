c9bfdca3556c3bac55134c0dc1f52f232d94832a
Sivasankari <sivasankarisivaraj1@gmail.com>
2023-02-24 13:28:22 +0530

    Restructuring of Project folder

---
+++ index.php
@@ -0,0 +1,129 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang = "en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Sivasankari | Web Developer</title>
+
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="style.css" />
+        <!-- <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Sofia+Sans:wght@400;600;800&display=swap" rel="stylesheet"> -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&display=swap" rel="stylesheet">
+    </head>
+
+<body>
+<div id = "main">
+<nav>
+      <a class = "logolink" href ="#" ><img class = "logo" src = "Sivasankarisharplogo.png" alt = "Sivasankari" /></a>
+      <ul class="navigation">
+        <li><a href="#about" class="nav-link">ABOUT</a></li>
+        <li><a href="#skills" class="nav-link">SKILLS</a></li>
+        <li><a href="#projects" class="nav-link">PROJECTS</a></li>
+        <a class = "ctabutton" href = "#" ><button class="cta" id="contact">CONTACT</button></a>
+        <!--<li><a href="#contact" class="nav-link">CONTACT</a></li>-->
+      </ul>
+      <button class="burger-menu" id="burger-menu">
+        <i class="fa fa-bars" name="menu-outline"></i>
+      </button>
+</nav> 
+<div class = "hero" id = "about">
+     <div class="bio">
+        <p class="bio-text">
+          <h5>Hi !</h5>
+          <!-- <div class = "intro">
+          <h4>I am</h4>
+          <h2>&nbsp;Sivasankari</h2>
+          </div> -->
+          <div class = "intro">
+            <h1 class = "iam">
+              I'm 
+            </h1> 
+            &nbsp; 
+            <h1>
+              Sivasankari
+            </h1>  
+          </div>
+          <h3 class = "flicker">Web Developer</h3><br>
+          <!-- <h6>I build beautiful responsive websites for businesses and am very passionate about coding.</h6> -->
+        </p>
+      </div> 
+</div> 
+</div>
+
+<section class = "more-about">
+<div class="about">
+        <div class="about-title">
+        <h1>ABOUT ME</h1>
+        </div>
+        <p class="about-text">
+        I am a full stack developer passionate about coding.I build beautiful responsive websites 
+        and help brands to establish strong digital presence.
+        </p>
+      </div>
+</section>
+
+<section class="skills" id="skills">
+      <h2 class="skill-header">My Top Skills</h2>
+
+      <div class="skills-wrapper">
+        <div class="first-set animate__animated animate__pulse">
+          <img
+            src="html5.png"
+            alt=""
+            loading="lazy"
+            class="icon icon-card"
+          />
+          <img
+            src="css3.png"
+            alt=""
+            loading="lazy"
+            class="icon icon-card"
+          />
+          <img
+            src="javascript.png"
+            alt=""
+            loading="lazy"
+            class="icon icon-card"
+          />
+        </div>
+
+        <div class="second-set animate__animated animate__pulse">
+          <img
+            src="php.png"
+            alt=""
+            loading="lazy"
+            class="icon icon-card"
+          />
+          <img
+            src="mysql.png"
+            alt=""
+            loading="lazy"
+            class="icon icon-card"
+          />
+          <img
+            src="wordpress.png"
+            alt=""
+            loading="lazy"
+            class="icon icon-card"
+          />
+        </div>
+      </div>
+    </section> 
+    <section>
+</section>
+
+</body>
+
+
+
+
+
+</html>
